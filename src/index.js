4c81ad11d6dde331b4d2ce6cb428cc2f46c72381
Alexander Repeta <alexander.repeta@gmail.com>
2022-04-18 21:11:57 +0300

    add eamples

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -8,7 +8,7 @@ import { store, persistor } from './redux/store';
 
 const theme = {};
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -17,6 +17,5 @@ ReactDOM.render(
         </ThemeProvider>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
